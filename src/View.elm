2caf10e7b2ff18d1768941ca44a67696a58e586c
Justus <mail@justusperlwitz.com>
2017-07-30 01:36:04 +0200

    Add footer

--- src/View.elm
+++ src/View.elm
@@ -7,8 +7,26 @@ import View.Home
 import Model.PageState exposing (Page(..))
 
 
+viewNav : Html Msg
+viewNav =
+    nav []
+        [ p [] [ text "This is the nav bar" ]
+        ]
+
+
+viewFooter : Html Msg
+viewFooter =
+    footer []
+        [ p [] [ text "This is the footer" ]
+        ]
+
+
 view : Model -> Html Msg
 view model =
-    case model.pageState.page of
-        Home ->
-            View.Home.view model
+    div []
+        [ viewNav
+        , case model.pageState.page of
+            Home ->
+                View.Home.view model
+        , viewFooter
+        ]
