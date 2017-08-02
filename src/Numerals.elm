20fa61478882bf90270ca6d1b0257cdaf4e7eb7b
Justus <mail@justusperlwitz.com>
2017-08-03 01:47:31 +0200

    Add tests and restructure code

---
+++ src/Numerals.elm
@@ -0,0 +1,123 @@
+module Numerals exposing (arabicToJapanese)
+
+import Dict
+
+
+toDigit : Int -> Char -> ( Maybe Char, Maybe Char )
+toDigit position number =
+    let
+        greaterOne =
+            case number of
+                '0' ->
+                    Nothing
+
+                '1' ->
+                    Nothing
+
+                _ ->
+                    Just number
+
+        greaterZero =
+            case number of
+                '0' ->
+                    Nothing
+
+                _ ->
+                    Just number
+
+        mapZero digit =
+            ( greaterZero
+            , Just digit
+            )
+
+        mapSignificant digit =
+            case number of
+                '0' ->
+                    ( Nothing, Nothing )
+
+                '1' ->
+                    ( Nothing, Just digit )
+
+                _ ->
+                    ( greaterZero, Just digit )
+    in
+        case position % 4 of
+            0 ->
+                case position // 4 of
+                    0 ->
+                        ( greaterZero
+                        , Nothing
+                        )
+
+                    1 ->
+                        mapZero '万'
+
+                    2 ->
+                        mapZero '億'
+
+                    3 ->
+                        mapZero '兆'
+
+                    4 ->
+                        mapZero '京'
+
+                    _ ->
+                        ( Nothing, Nothing )
+
+            1 ->
+                mapSignificant '十'
+
+            2 ->
+                mapSignificant '百'
+
+            3 ->
+                mapSignificant '千'
+
+            _ ->
+                ( Nothing, Nothing )
+
+
+arabicToJapanese : String -> String
+arabicToJapanese arabic =
+    let
+        digits =
+            Dict.fromList
+                [ ( '0', '○' )
+                , ( '1', '一' )
+                , ( '2', '二' )
+                , ( '3', '三' )
+                , ( '4', '四' )
+                , ( '5', '五' )
+                , ( '6', '六' )
+                , ( '7', '七' )
+                , ( '8', '八' )
+                , ( '9', '九' )
+                ]
+
+        convertNumber ( a, b ) =
+            ( a |> Maybe.andThen (\a -> Dict.get a digits)
+            , b
+            )
+
+        collapse ( a, b ) =
+            case ( a, b ) of
+                ( Just number, Just digit ) ->
+                    [ number, digit ]
+
+                ( Nothing, Just digit ) ->
+                    [ digit ]
+
+                ( Just number, Nothing ) ->
+                    [ number ]
+
+                ( Nothing, Nothing ) ->
+                    []
+    in
+        arabic
+            |> String.toList
+            |> List.reverse
+            |> List.indexedMap toDigit
+            |> List.map convertNumber
+            |> List.map collapse
+            |> List.foldl (++) []
+            |> String.fromList
--- tests/Example.elm
+++ tests/Example.elm
@@ -3,8 +3,21 @@ module Example exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, list, int, string)
 import Test exposing (..)
+import Numerals
+
+
+expectArabicToJapanese a b =
+    test a <| \_ -> Expect.equal b (Numerals.arabicToJapanese a)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    describe "arabicToJapanese"
+        [ expectArabicToJapanese "17" "十七"
+        , expectArabicToJapanese "151" "百五十一"
+        , expectArabicToJapanese "302" "三百二"
+        , expectArabicToJapanese "469" "四百六十九"
+        , expectArabicToJapanese "2025" "二千二十五"
+        , expectArabicToJapanese "9836703" "九百八十三万六千七百三"
+        , expectArabicToJapanese "2036521801" "二十億三千六百五十二万千八百一"
+        ]
